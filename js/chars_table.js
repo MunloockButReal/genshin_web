7667b1d5cf11c144c6ce762e1250187bc6c3d32b
Munloock <munloock@gmail.com>
2022-11-15 14:53:50 +0200

    formatting

--- js/chars_table.js
+++ js/chars_table.js
@@ -1,56 +1,17 @@
 // File reader
-/*
-var url = "../htmls/chars.json";
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
-xhr.send();
-
 let charData;
-xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-            var result = JSON.parse(xhr.responseText);
-            charData = result;
-        }
-    }
-};
 
-
-var charData;
-function dataRead() {
-var url = "../htmls/chars.json";
-fetch(url).then((response) => 
-{ return response.text(); }).then((data) => 
-{ let charData = data });
-}
-
-
-function readData() {
-let response = fetch(url);
-
-if (response.ok) { // если HTTP-статус в диапазоне 200-299
-  // получаем тело ответа (см. про этот метод ниже)
-  let charData = response.json();
-} else {
-  alert("Ошибка HTTP: " + response.status);
-}
-}*/
-
-        let charData;
-
-        $.ajax({
-            url: "../htmls/chars.json",
-            dataType: 'json',
-            async: false,
-            success: function (data) {
-                charData = data;
-            }
-        });
-
-
-// Dynamic table
+$.ajax({
+    url: "../htmls/chars.json",
+    dataType: "json",
+    async: false,
+    success: function (data) {
+        charData = data;
+    },
+});
 
 window.onload = () => {
+    // Dynamic table
     loadTableData(charData);
     //sort and color HP
     sortTable(3);
@@ -85,21 +46,23 @@ function loadTableData(charData) {
     tableBody.innerHTML = dataHtml;
 }
 let colorsArray = [
-    "#aa0000", "#ae0c00", "#b31800", "#b72400", "#bb3000", "#bf3d00", "#c44900", "#c85500",
-    "#cc6100", "#d06d00", "#d57900", "#d98500", "#dd9100", "#e19d00", "#e6a900", "#eab600",
-    "#eec200", "#f2ce00", "#f7da00", "#fbe600", "#fff200", "#f2ee00", "#e6e900", "#d9e500",
-    "#cce000", "#bfdc00", "#b3d700", "#a6d300", "#99ce00", "#8cca00", "#80c600", "#73c100",
-    "#66bd00", "#59b800", "#4db400", "#40af00", "#33ab00", "#26a600", "#1aa200",
-];
+    "#aa0000", "#ae0c00", "#b31800", "#b72400", "#bb3000", "#bf3d00",
+    "#c44900", "#c85500", "#cc6100", "#d06d00", "#d57900", "#d98500",
+    "#dd9100", "#e19d00", "#e6a900", "#eab600", "#eec200", "#f2ce00",
+    "#f7da00", "#fbe600", "#fff200", "#f2ee00", "#e6e900", "#d9e500",
+    "#cce000", "#bfdc00", "#b3d700", "#a6d300", "#99ce00", "#8cca00",
+    "#80c600", "#73c100", "#66bd00", "#59b800", "#4db400", "#40af00",
+    "#33ab00", "#26a600", "#1aa200"];
+
 function colorcode(columnName) {
     var f = document.getElementsByClassName(columnName);
     var min = Math.round(parseInt(f[0].innerHTML) * 0.95);
     var max = Math.round(parseInt(f[f.length - 1].innerHTML) * 1.05);
 
-    step = Math.round(((max - min) / colorsArray.length)); // step for minmax color
-    let currentmin = min; // Minimum for color
-    let current = 0; // current
-    let colorNumber = 0; // Color number from p[]
+    step = Math.round((max - min) / colorsArray.length);
+    let currentmin = min;
+    let current = 0;
+    let colorNumber = 0;
 
     for (l = 0; l < f.length; l++) {
         current = f[l].innerHTML;
@@ -118,7 +81,6 @@ function colorcode(columnName) {
         }
     }
 }
-
 
 function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
