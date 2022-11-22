68ae8e4823cbe93c7d1bbcc630791250be6b6809
Munloock <munloock@gmail.com>
2022-11-22 15:58:11 +0200

    Update char_bd.js: Make function for adding events for images

--- js/char_bd.js
+++ js/char_bd.js
@@ -1,21 +1,18 @@
-        let characterLinks;
+let characterLinks;
 
-        window.onload = () => {
-            if (!document.querySelector('table')){
-            document.body.style.width = document.querySelector('table').offsetWidth + 'px';
-            document.body.style.height = document.querySelector('table').offsetHeight + document.querySelector('div').offsetHeight + 'px';
-        }}
+window.onload = () => {};
 
+function addEventsOnChars() {
+	$.ajax({
+		url: '../js/chars.json',
+		dataType: 'json',
+		async: false,
+		success: function (data) {
+			characterLinks = data;
+		},
+	});
 
-        $.ajax({
-            url: "../js/chars.json",
-            dataType: 'json',
-            async: false,
-            success: function (data) {
-                characterLinks = data;
-            }
-        });
-
-        $("img").click(function () {
-            window.open(characterLinks[$(this).attr("alt")]);
-        });
+	$('img').click(function () {
+		window.open(characterLinks[$(this).attr('alt')]);
+	});
+}
